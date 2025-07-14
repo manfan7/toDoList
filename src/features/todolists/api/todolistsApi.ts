b4bc6a5df97f2b6aee8ca8198fc1645dba4fcbb7
Hrytsuki <igorgrytsuk@yandex.ru>
2025-07-15 00:21:11 +0300

    add first changes to transimit to rtquery

--- src/features/todolists/api/todolistsApi.ts
+++ src/features/todolists/api/todolistsApi.ts
@@ -1,7 +1,30 @@
-import { instance } from "@/common/instance/instance.ts"
-import type { BaseResponseZod, DefaultResponse, toDoList } from "@/common/types"
+import {instance} from "@/common/instance/instance.ts"
+import type {BaseResponseZod, DefaultResponse, toDoList} from "@/common/types"
+import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
+import {AUTH_TOKEN} from "@/common/constants";
 
-export const todolistsApi = {
+export const todoListApi = createApi({
+  reducerPath: 'todoListApi',
+  baseQuery: fetchBaseQuery({
+    baseUrl: import.meta.env.VITE_BASE_URL,
+    prepareHeaders: (headers) => {
+      headers.set("API-KEY", import.meta.env.VITE_API_KEY);
+      headers.set('Authorization', `Bearer ${localStorage.getItem(AUTH_TOKEN)}`);
+      return headers;
+    }
+  }),
+  endpoints: (builder) => ({
+    getToDoLists: builder.query<any, void>({
+      query: () => ({
+        url: '/todo-lists',
+        method: 'GET'
+      })
+    })
+  })
+});
+
+
+export const _todolistsApi = {
   getToDoList() {
     return instance.get<toDoList[]>("/todo-lists")
   },
