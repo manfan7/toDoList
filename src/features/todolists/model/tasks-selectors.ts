b53f7b54cda6a65afaa69722638c56947ef5403e
Hrytsuki <igorgrytsuk@yandex.ru>
2025-07-26 00:40:12 +0300

    3 urok last changes

--- src/features/todolists/model/tasks-selectors.ts
+++
@@ -1,6 +0,0 @@
-/*
-import { RootState } from "../../../app/store.ts"
-import type { tasksListType } from "@/common/types"
-
-export const selectTasks = (state: RootState): tasksListType => state.tasks
-*/
--- src/features/todolists/model/toDoSelectors.ts
+++
@@ -1,4 +0,0 @@
-import { RootState } from "@/app/store.ts"
-import { type toDotype } from "@/App.tsx"
-
-export const selectToDoList = (state: RootState): toDotype[] => state.todoLists.todo
